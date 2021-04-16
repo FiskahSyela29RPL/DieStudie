50b4da86db607ec08f4f42a09a81ebea358f1516
Fiskah Syela <72687804+FiskahSyela29RPL@users.noreply.github.com>
2021-04-16 17:32:39 +0700

    Berisi jawaban looping 2

---
+++ src/Introduction/Solusi6.java
@@ -0,0 +1,23 @@
+package ObjectOriented;
+import java.util.*;
+import java.io.*;
+
+//looping 2 java dasar
+public class Solusi{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int a=in.nextInt();
+        for(int i=0;a<t;i++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int z = in.nextInt();
+            int b = x;
+            for(int j=0;j<z;j++){
+                b += Math.pow(2, j)*y;
+                System.out.printf("%s ",b);
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
